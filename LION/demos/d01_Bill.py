Correct calculation for number of incident photons - sinogram is already non-log corrected

LION/demos/d01_Bill.py
@@ -11,7 +11,6 @@ import numpy as np
 import torch
 import tomosipo as ts
 import matplotlib.pyplot as plt
-import AItomotools.CTtools.ct_utils as ct
 import nibabel as nib
 from ts_algorithms import fbp
 """
@@ -26,28 +25,30 @@ doi: 10.1097/RCT.0b013e318258e891
 def from_normal_to_HU(image):
     return np.maximum((image*3000)-1000,-1000)
 
-image = np.rot90(nib.load('/media/mcgoug01/nvme/Data/kits19_phases/noncontrast/KiTS-00000.nii.gz').get_fdata()[:,:,55],3)
-normal_image = np.expand_dims(ct.from_HU_to_normal(image),0)
+def from_HU_to_normal(image):
+    return np.maximum((image+1000)/3000,0)
 
-dose_fraction = 0.5
+image = np.rot90(nib.load('/media/mcgoug01/nvme/Data/kits19_phases/noncontrast/KiTS-00000.nii.gz').get_fdata()[:,:,55],3)
+normal_image = np.expand_dims(from_HU_to_normal(image),0)
+
+dose_fraction = 0.2
 num_detectors = 3600
-electronic_noise_sigma = 1
+electronic_noise_sigma = 0.1
 angles = 360
 
 vg = ts.volume(shape=normal_image.shape, size=(5, 300, 300))
 pg = ts.parallel(angles=angles, shape=(1, num_detectors), size=(1, num_detectors))
 A = ts.operator(vg, pg)
 
-sino = A(normal_image)
-
+expP = A(normal_image) # A() produces non-log transformed sinogram, hence expP
 air = normal_image[:, 100:150, 150:350]
 
 dist = np.random.normal(loc=air.mean(), scale=air.std(), size=normal_image.shape)
-P = A(dist)
-Noa = 1/np.var(np.exp(-P)) # number of incident photons! this is estimated using an air sample
+expPair = A(dist) # this is expP of an equivalent 'air' sample, taken from original image
+Noa = 1 / np.var(np.exp((np.log(expPair+1)*-1 - np.log(1)))) # number of incident photons! this is estimated using the air sino
 print(Noa)
 
-mu, sigma = len(sino)/2, 5 # mean and standard deviation
+mu, sigma = len(expP)/2, 5 # mean and standard deviation
 s = np.random.normal(mu, sigma, 10000)
 count, bins, ignored = plt.hist(s, num_detectors-1, density=True)
 plt.close()
@@ -56,15 +57,18 @@ bowtie /= bowtie.max()*1.2
 bowtie+=+0.16666666667
 constant = (1-dose_fraction)/dose_fraction
 
-electronic_noise = 1+((electronic_noise_sigma**2)*constant*sino/Noa)
+electronic_noise_stddev = np.sqrt(1+((1+dose_fraction)/dose_fraction)*(electronic_noise_sigma**2)*expP/Noa)
+quantum_noise_stddev = np.sqrt(constant * (expP/Noa))
 
-normal = np.random.normal(size=sino.shape)
-noise = np.sqrt(constant * (sino/Noa)*electronic_noise) * normal
+print(electronic_noise_stddev.mean()-1,quantum_noise_stddev.mean())
+
+normal = np.random.normal(size=expP.shape)
+noise = quantum_noise_stddev * quantum_noise_stddev * normal
 filtered_noise = (bowtie*noise)
-sino_noisy = filtered_noise+sino
+sino_noisy = filtered_noise+expP
 
 recon_noise = from_normal_to_HU(fbp(A,torch.Tensor(filtered_noise)).detach().cpu().numpy())[0]
-recon_og = from_normal_to_HU(fbp(A,torch.Tensor(sino)).detach().cpu().numpy())[0]
+recon_og = from_normal_to_HU(fbp(A,torch.Tensor(expP)).detach().cpu().numpy())[0]
 recon_sLDCT = from_normal_to_HU(fbp(A,torch.Tensor(sino_noisy)).detach().cpu().numpy())[0]
 
 plt.figure(figsize=(18,6))